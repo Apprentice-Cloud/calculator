b6f728f872bd2f80c45847bfa9d363d0fed209b3
Lesley <leskimuti@gmail.com>
2022-06-24 21:17:08 +0300

    First commit - calculator app

---
+++ js/main.js
@@ -0,0 +1,33 @@
+// *** Write your code here***
+// let btns = document.querySelectorAll("input");
+// let clearDisplay = document.querySelector(".clear");
+// let secondNums;
+
+// let display = btns[0];
+// console.log(display);
+
+// // clearDisplay.addEventListener("click", clear());
+
+// btns.forEach((btn) => {
+//   if (btn.type === "button" && btn.value != "C" && btn.value != "=") {
+//     btn.addEventListener("click", () => {
+//       display.value += btn.value;
+//       console.log(display.value);
+//     });
+//   } else if (btn.value === "C") {
+//     btn.addEventListener("click", clear());
+//   }
+// });
+
+// function clear() {
+//   display.value = "";
+//   console.log(display.value);
+// }
+
+// function delete(){
+
+// }
+
+// function calcNumbers() {
+
+// }
